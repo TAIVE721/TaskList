088a3a89146f4ae64a5fad9b4f33aefd20ced643
TAIVE <davidcorachuctaya@gmail.com>
2025-08-30 19:01:16 -0500

    yeeeee

--- src/main/java/com/nekotasks/arc/repositories/TaskRepository.java
+++ src/main/java/com/nekotasks/arc/repositories/TaskRepository.java
@@ -11,6 +11,7 @@ import com.nekotasks.arc.domain.entities.Task;
 public interface TaskRepository extends JpaRepository<Task, UUID> {
     List<Task> findByTaskListId(UUID taskListId);
 
-    Optional<Task> finByTaskListIdAndId(UUID taskListId, UUID id);
+    Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
 
+    void deleteByTaskListAndId(UUID taskListId, UUID id);
 }
---
+++ src/main/java/com/nekotasks/arc/services/TaskService.java
@@ -0,0 +1,19 @@
+package com.nekotasks.arc.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.nekotasks.arc.domain.entities.Task;
+
+public interface TaskService {
+    List<Task> listTasks(UUID taskListId);
+
+    Task createTask(UUID taskListId, Task task);
+
+    Optional<Task> getTask(UUID taskListId, UUID taskId);
+
+    Task updateTask(UUID taskListId, UUID taskId, Task task);
+
+    void deleteTask(UUID taskListId, UUID taskId);
+}
